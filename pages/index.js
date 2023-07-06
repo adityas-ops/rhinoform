046948464d5a7a04ab0abcfeb65e5c7dd439b10d
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-07-06 21:28:18 +0530

    as

--- pages/index.js
+++ pages/index.js
@@ -34,10 +34,8 @@ export default function Home() {
 
   // for experience
 
-  //  achievements 
-  const [achievelist, setAchievelist] = useState([
-    { achieve: '' },
-  ]);
+  //  achievements
+  const [achievelist, setAchievelist] = useState([{ achieve: "" }]);
 
   const handleInputChangeA = (index, e) => {
     const { name, value } = e.target;
@@ -47,10 +45,7 @@ export default function Home() {
   };
 
   const handleAddEducationA = () => {
-    setAchievelist([
-      ...achievelist,
-      {achieve: '' },
-    ]);
+    setAchievelist([...achievelist, { achieve: "" }]);
   };
 
   const handleDeleteEducationA = (index) => {
@@ -63,7 +58,9 @@ export default function Home() {
     <>
       <div className="w-full h-full font-jetBrain  overflow-y-scroll">
         {/* header part */}
-        <div className="w-full h-[60px] border-b bg-white z-50 border-zinc-300 fixed"></div>
+        <div className="w-full h-[60px] border-b bg-white z-50 border-zinc-300 text-3xl font-bold flex items-center justify-center fixed">
+          Header
+        </div>
         <div className=" mt-[60px] w-full h-full flex justify-center">
           <form
             onSubmit={handleSubmit}
@@ -170,8 +167,8 @@ export default function Home() {
               Education
             </h2>
             {/* <div className="pt-6"> */}
-              {educationList.map((education, index) => (
-                <div key={index} className="py-8 border-b border-zinc-300"  >
+            {educationList.map((education, index) => (
+              <div key={index} className="py-8 border-b border-zinc-300">
                 <div className="mb-4">
                   <label className="text-[1.125rem] font-[400]">
                     College name <span className="text-red-500">*</span>
@@ -181,90 +178,97 @@ export default function Home() {
                     required
                     className="w-full py-2 mt-3 px-3 bg-[#F6F6F6]  border border-zinc-300"
                   />
-                  </div>
-                  <div className="mb-4">
+                </div>
+                <div className="mb-4">
                   <label className="text-[1.125rem] font-[400]">
-                   Degree <span className="text-red-500">*</span>
+                    Degree <span className="text-red-500">*</span>
                   </label>
                   <input
                     type="text"
                     required
                     className="w-full py-2 mt-3 px-3 bg-[#F6F6F6]  border border-zinc-300"
                   />
+                </div>
+                <div className="w-full grid lg:grid-cols-2 grid-cols-1  gap-4 pt-2">
+                  <div className="flex items-center">
+                    <label className="lg:text-[1.125rem] text-[14px] font-[400]">
+                      Start date <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="date"
+                      required
+                      className="py-2 mt-3 ml-3  px-3  bg-[#F6F6F6]  border border-zinc-300"
+                    />
                   </div>
-                  <div className="flex justify-between pt-2">
-                    <div className="flex items-center">
-                      <label className="text-[1.125rem] font-[400]">
-                        Start date <span className="text-red-500">*</span>
-                      </label>
-                      <input
-                        type="date"
-                        required
-                        className="py-2 mt-3 ml-3 px-3 bg-[#F6F6F6]  border border-zinc-300"/>
-                    </div>
-                    <div className="flex items-center">
-              <label className="text-[1.125rem] font-[400]">
-                End date <span className="text-red-500">*</span>
-              </label>
-              <input
-                type="date"
-                required
-                className="
+                  <div className="flex items-center">
+                    <label className="lg:text-[1.125rem] text-[14px] font-[400]">
+                      End date <span className="text-red-500">*</span>
+                    </label>
+                    <input
+                      type="date"
+                      required
+                      className="
                 py-2 mt-3 ml-3 px-3 bg-[#F6F6F6]  border border-zinc-300"
-              />
-              </div>
+                    />
                   </div>
                 </div>
-              ))}
-             <div className="pt-8 w-full flex justify-between">
-             <button type="button"
-          className="bg-zinc-300  px-4 py-2 w-[40%] rounded mb-4 hover:bg-zinc-200"
-          onClick={handleAddEducation}
-          >Add Education</button>
-          {
-            educationList.length > 1 && (
+              </div>
+            ))}
+            <div className="pt-8 w-full  grid lg:grid-cols-2 gap-4 grid-cols-1 ">
               <button
-              type="button"
-              className="bg-zinc-300  px-4 py-2 w-[40%] rounded mb-4 hover:bg-zinc-200"
-              onClick={handleDeleteEducation}
-              >Remove Education</button>
-            )
-          }
-          {/*  achievement start */}
-             </div>
-             {/* achievement start */}
+                type="button"
+                className="bg-zinc-300  px-4 py-2  w-full rounded mb-4 hover:bg-zinc-200 whitespace-nowrap"
+                onClick={handleAddEducation}
+              >
+                Add Education
+              </button>
+              {educationList.length > 1 && (
+                <button
+                  type="button"
+                  className="bg-zinc-300  px-4 py-2  w-full whitespace-nowrap rounded mb-4 hover:bg-zinc-200"
+                  onClick={handleDeleteEducation}
+                >
+                  Remove Education
+                </button>
+              )}
+              {/*  achievement start */}
+            </div>
+            {/* achievement start */}
             <h2 className="text-[1.25rem] font-[600] mt-10 pt-2 border-t border-zinc-300">
-             Achievement
+              Achievement
             </h2>
             <div className="pt-6">
               {achievelist.map((achieve, index) => (
-                <div key={index} className="py-1 "  >
-                <div className="mb-3 flex items-center ">
-                <p className="mr-4">{index+1}.</p>
-                  <input
-                    type="text"
-                    required
-                    className="w-full py-2 mt-3 px-3 bg-[#F6F6F6]  border border-zinc-300"
-                  />
+                <div key={index} className="py-1 ">
+                  <div className="mb-3 flex items-center ">
+                    <p className="mr-4">{index + 1}.</p>
+                    <input
+                      type="text"
+                      required
+                      className="w-full py-2 mt-3 px-3 bg-[#F6F6F6]  border border-zinc-300"
+                    />
                   </div>
-                    </div>
+                </div>
               ))}
-             <div className="pt-8 w-full flex justify-between">
-             <button type="button"
-          className="bg-zinc-300  px-4 py-2 w-[40%] rounded mb-4 hover:bg-zinc-200"
-          onClick={handleAddEducationA}
-          >Add Achievement</button>
-          {
-            achievelist.length > 1 && (
-              <button
-              type="button"
-              className="bg-zinc-300  px-4 py-2 w-[40%] rounded mb-4 hover:bg-zinc-200"
-              onClick={handleDeleteEducationA}
-              >Remove Achievement</button>
-            )
-          }
-          </div>
-          </div>
+              <div className="pt-8 w-full  grid lg:grid-cols-2 gap-4 grid-cols-1">
+                <button
+                  type="button"
+                  className="bg-zinc-300  px-4 py-2 w-full rounded mb-4 hover:bg-zinc-200"
+                  onClick={handleAddEducationA}
+                >
+                  Add Achievement
+                </button>
+                {achievelist.length > 1 && (
+                  <button
+                    type="button"
+                    className="bg-zinc-300  px-4 py-2 w-full rounded mb-4 hover:bg-zinc-200"
+                    onClick={handleDeleteEducationA}
+                  >
+                    Remove Achievement
+                  </button>
+                )}
+              </div>
+            </div>
             {/* achievement end */}
             {/* file upload cv */}
             <div className="pt-4 mt-4 border-t border-zinc-300">
@@ -278,6 +282,22 @@ export default function Home() {
               />
             </div>
             {/* submit button */}
+            <div className="pt-4 mt-4 border-t border-zinc-300">
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="bg-[#4611EA] text-white px-4 py-3 w-[20%] rounded mb-4 hover:opacity-70 flex items-center justify-evenly"
+              >
+                Submit
+               <p className="w-[15px] h-[15px] font-bold"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
+                  <path
+                    style={{ fill: "#fff",strokeWidth:"33"}}
+                    d="m17.5 5.999-.707.707 5.293 5.293H1v1h21.086l-5.294 5.295.707.707L24 12.499l-6.5-6.5z"
+                    data-name="Right"
+                  />
+                </svg></p>
+              </button>
+            </div>
           </form>
         </div>
       </div>
